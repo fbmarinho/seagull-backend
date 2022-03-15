71b4226bc77c697c6cc9edf63b7807c0aeede875
Felipe Marinho <felipegigamail@gmail.com>
2022-03-14 21:34:18 -0300

    conditional dotenv require

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (!process.env.NODE_ENV) {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
