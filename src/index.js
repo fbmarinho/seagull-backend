28f217a2332e433082a17f0f877d0b3e61f5c682
Felipe Marinho <felipegigamail@gmail.com>
2022-03-15 02:41:23 -0300

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ if (!process.env.NODE_ENV) {
   require("dotenv").config();
 }
 const sondas = require("./Routes/sonda");
-const MongoClient = require("mongodb").MongoClient;
 
 const express = require("express");
 const app = express();
@@ -18,21 +17,7 @@ app.use(myLogger); //Middleware
 app.use("/sondas", sondas);
 
 app.get("/", (req, res) => {
-  MongoClient.connect(
-    "mongodb+srv://backend:cu8iwpR2bbiSjwpM@seagull.goppk.mongodb.net/db?retryWrites=true&w=majority",
-    function (err, client) {
-      if (err) throw err;
-
-      var db = client.db("db");
-
-      db.collection("sondas").find({}, function (findErr, result) {
-        if (findErr) throw findErr;
-        console.log(result);
-        res.json(result);
-        client.close();
-      });
-    }
-  );
+  res.send("Hello World !");
 });
 
 app.listen(port, () => {
